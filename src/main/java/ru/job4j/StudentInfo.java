5f18bf76b8b730ed266207318518725732ff7242
vg52 <v.gabrusenok10@gmail.com>
2026-04-12 15:03:29 +0300

    4.1. Разбор первой программы. [#159033 #66433]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Vlad Vlados");
+        System.out.println("25.01.1998");
+    }
+}
